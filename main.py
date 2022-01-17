132dc35737a970cd6675b6af9038df1fa16acc8e
Muhammet Şen <enmuhammet23@gmail.com>
2022-01-17 14:43:07 +0300

    Added comments

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def find_myself(column, row, sqrt_cell):
 
 if rank == 0:  # manager
     filename = argv[1]
-    with open(f"input-output/{filename}") as f:
+    with open(filename) as f:
         N, wave, c = [int(x) for x in f.readline().split()]
         future_towers = f.readlines()
-
         f.close()
+
     number_of_cells_per_worker = N ** 2 / P
     sqrt_cell = int(math.sqrt(number_of_cells_per_worker))
-    game_map = [[{"row": x, "col": y, "cellIndex": (x)*N+y+1, "type": ".", "health": 0, "host": (x//sqrt_cell)*column_size+(y//sqrt_cell+1)}
+    game_map = [[{"row": x, "col": y, "type": ".", "health": 0}
                  for y in range(N)]for x in range(N)]
 
     sqrt_p = int(math.sqrt(P))
@@ -55,7 +55,6 @@ if rank == 0:  # manager
 
     for i in range(wave):
         wave_count = i
-        # add health of towers got can
         process_towers = [[] for x in range(size)]
         o_tower = o_s[i]
         plus_towers = plus_s[i]
@@ -66,72 +65,42 @@ if rank == 0:  # manager
         for plus in plus_towers:
             process_towers[find_myself(
                 plus["col"], plus["row"], sqrt_cell)-1].append(plus)
-        # print(process_towers)
         for ad1 in range(1, P+1):
             comm.send(process_towers[ad1-1], dest=ad1, tag=22)
-    final_maps = {}
-    for ad2 in range(1, P+1):
-        final_maps[ad2-1] = comm.recv(source=ad2, tag=100)
-    # print(f"{final_maps=}")
 
-    # print(f"wave {wave_count+1} basi:")
+    final_maps = {}
+    for j in range(1, P+1):  # get maps from workers at the end of the game
+        final_maps[j-1] = comm.recv(source=j, tag=100)
+
     game_map_final = [[{} for y in range(N)] for x in range(N)]
-    for r in range(sqrt_p):  # 0 1 2 3
-        for c in range(sqrt_p):  # 0 1 2 3
+    for r in range(sqrt_p):
+        for c in range(sqrt_p):
             p = r * row_size + c
-            # print(f"{p=} {r=} {c=} ")
-            for a in range(sqrt_cell):  # 0 1 2
+            for a in range(sqrt_cell):
                 for t in final_maps[p][a]:
-                    # print(t['type'], end=" ")
                     r_1 = t['row']
                     c_1 = t['col']
                     game_map_final[r_1][c_1] = t['type']
 
-    # for x in range(N):
-    #     print(" ".join(game_map_final[x]),end="")
-    print("\n".join([" ".join(game_map_final[x])for x in range(N)]),end="")
-        # for asd in range(8):
-        #     print(f"end of round {asd}")
-        #     for i in range(1, P+1):
-        #         final_maps[i-1] = comm.recv(source=i, tag=100)
-        #     game_map_final = [[{} for y in range(N)] for x in range(N)]
-        #     for r in range(sqrt_p):  # 0 1 2 3
-        #         for c in range(sqrt_p):  # 0 1 2 3
-        #             p = r * row_size + c
-        #             # print(f"{p=} {r=} {c=} ")
-        #             for a in range(sqrt_cell):  # 0 1 2
-        #                 for t in final_maps[p][a]:
-        #                     # print(t['type'], end=" ")
-        #                     r_1 = t['row']
-        #                     c_1 = t['col']
-        #                     game_map_final[r_1][c_1] = f"{t['type']}"
-
-        #     for x in range(N):
-        #         for y in range(N):
-        #             print(game_map_final[x][y], end=" ")
-        #         print()
+    print("\n".join([" ".join(game_map_final[x]) for x in range(N)]), end="")
 
 
 else:
-    # print(f"{rank=}")
-    # you are a workerrr
     data = comm.recv(source=0, tag=0)
     row = data["row"] + 1
-    wave = data["wave"]
     column = data["column"] + 1
+    wave = data["wave"]
     part_map = data["part_map"]
     sqrt_cell = len(part_map[0])
     N = sqrt_cell*(P**(1/2))
 
     for i in range(wave):
+        # start of wave
         myself_towers = comm.recv(source=0, tag=22)
         for tower in myself_towers:
             if part_map[tower["row"] % sqrt_cell][tower["col"] % sqrt_cell]["type"] == ".":
                 part_map[tower["row"] %
                          sqrt_cell][tower["col"] % sqrt_cell] = tower
-
-        # start of wave
-        # comm.send(part_map, dest=0, tag=100)  # to print first wave
 
         for i in range(8):
             # start of round
@@ -139,83 +108,55 @@ else:
             bottom_neighbour = None
             left_neighbour = None
             right_neighbour = None
-            # Horizontal
-        #    tek rowlar alt kenarlarını alttaki çifte versin, alttaki çiften kenar beklesin
 
+            # Horizontal
+            #   odd rows send bottom side to below even and wait response from it
             if row % 2 == 1:
                 target = rank + row_size
-                if (1 <= target <= P):
-
+                if (1 <= target <= P):  # check if it is in boundries
                     comm.send(part_map[-1], dest=target, tag=2)
-                    # print(f"S {rank} {target}")
-
                     bottom_neighbour = comm.recv(source=target, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
             else:
                 source = rank - row_size
-
                 if (1 <= source <= P):
-                    # print(f"W {rank} {source}")
-
                     top_neighbour = comm.recv(source=source, tag=2)
                     comm.send(part_map[0], dest=source, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
 
-        #    çift rowlar alt kenarlarını alttaki teke versin, alt rowdan kenar beklesin
-            #
+            #   even rows send bottom side to below odd and wait response from it
             if row % 2 == 0:
                 target = rank + row_size
                 if (1 <= target <= P):
-
                     comm.send(part_map[-1], dest=target, tag=2)
-                    # print(f"S {rank} {target}")
-
                     bottom_neighbour = comm.recv(source=target, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
             else:
                 source = rank - row_size
-
                 if (1 <= source <= P):
-                    # print(f"W {rank} {source}")
-
                     top_neighbour = comm.recv(source=source, tag=2)
                     comm.send(part_map[0], dest=source, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
 
-        #    tek columnlar sag kenarlarını sagdaki çifte versin, sagdaki çiften kenar beklesin
+            # We are transfering cross spots in two steps.
+            # While communicating horizontally, left and right sides send their top and bottom neighbours, if any, too.
+            # That's why we are using data_transfer_object to temporarly store those 2nd level neighbours
             data_transfer_object = {}
 
+            # odd columns send their right sides to right and wait response from it
             if column % 2 == 1:
                 target = rank + 1
                 if (rank % column_size != 0 and 1 <= target <= P):
 
-                    # operation a
-                    # print(f"S {rank} {target}")
-                    if top_neighbour != None:
+                    if top_neighbour != None:  # I have a top neighbour and i should send it to my right
                         data_transfer_object["top_neighbour"] = top_neighbour[-1]
-                    if bottom_neighbour != None:
+                    if bottom_neighbour != None:  # I have a bottom neighbour and i should send it to my right
                         data_transfer_object["bottom_neighbour"] = bottom_neighbour[-1]
-                    data_transfer_object["me"] = [x[-1] for x in part_map]
+                    data_transfer_object["me"] = [x[-1]
+                                                  for x in part_map]  # my right side
 
                     comm.send(data_transfer_object, dest=target, tag=2)
                     right_neighbour = comm.recv(source=target, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
+
             else:
                 source = rank - 1
-
                 if (rank % column_size != 1 and 1 <= source <= P):
-                    # print(f"W {rank} {source}")
-                    #  operation b
                     left_neighbour = comm.recv(source=source, tag=2)
                     if top_neighbour != None:
                         data_transfer_object["top_neighbour"] = top_neighbour[0]
@@ -223,36 +164,24 @@ else:
                         data_transfer_object["bottom_neighbour"] = bottom_neighbour[0]
                     data_transfer_object["me"] = [x[0] for x in part_map]
                     comm.send(data_transfer_object, dest=source, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
 
-        #    çift columnlar sag kenarlarını sagtaki teke versin, sag rowdan kenar beklesin
             data_transfer_object = {}
 
+            # even columns send their right sides to right and wait response from it
             if column % 2 == 0:
                 target = rank + 1
                 if (rank % column_size != 0 and 1 <= target <= P):
-
-                    # operation a
-                    # print(f"S {rank} {target}")
                     if top_neighbour != None:
                         data_transfer_object["top_neighbour"] = top_neighbour[-1]
                     if bottom_neighbour != None:
                         data_transfer_object["bottom_neighbour"] = bottom_neighbour[-1]
                     data_transfer_object["me"] = [x[-1] for x in part_map]
-
                     comm.send(data_transfer_object, dest=target, tag=2)
                     right_neighbour = comm.recv(source=target, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
+
             else:
                 source = rank - 1
-
                 if (rank % column_size != 1 and 1 <= source <= P):
-                    # print(f"W {rank} {source}")
-                    #  operation b
                     left_neighbour = comm.recv(source=source, tag=2)
                     if top_neighbour != None:
                         data_transfer_object["top_neighbour"] = top_neighbour[0]
@@ -260,25 +189,12 @@ else:
                         data_transfer_object["bottom_neighbour"] = bottom_neighbour[0]
                     data_transfer_object["me"] = [x[0] for x in part_map]
                     comm.send(data_transfer_object, dest=source, tag=1)
-                else:
-                    # print("Invalid attempt")
-                    pass
-
-            # print(f"""I am {rank=}. I have :"/
-            # \n\n top\n{top_neighbour}\n\n/
-            # \n\n bottom\n{bottom_neighbour}\n\n/
-            # \n\n left\n{left_neighbour}\n\n/
-            # \n\n right\n{right_neighbour}\n\n""")
 
             size = int(N / math.sqrt(P))
             area = [[{"type": "."} for y in range(size + 2)]
                     for i in range(size + 2)]  # 5x5
-            # area[1:-1][1:-1] = part_map
-            
-            # print(f"{N=} {P=}")
-            # print(size)
+
             for i in range(size):
-                # print(i)
                 area[i+1][1:-1] = part_map[i]
             if top_neighbour != None:
                 area[0][1:-1] = top_neighbour
@@ -299,14 +215,12 @@ else:
                 if "top_neighbour" in right_neighbour:
                     area[0][-1] = right_neighbour["top_neighbour"]
 
-            # print(f"""I am {rank=}. {area=}""")
-
-            # attaaaccck
+            # Attack
             attacks = [[0 for x in range(size+2)] for y in range(size+2)]
             for row in range(1, size+1):
                 for column in range(1, size+1):
                     tower = area[row][column]
-                    how_much_touched_my_ass_count = 0
+                    how_much_touched_me_count = 0
                     if tower["type"] == ".":
                         continue
                     elif tower["type"] == "+":
@@ -319,8 +233,8 @@ else:
                             area[row][column-1],
                             area[row+1][column-1],
                             area[row-1][column-1]]
-                        how_much_touched_my_ass_count += len(
-                            list(filter(lambda x: x["type"] == "o", nominees)))
+                        how_much_touched_me_count += len(
+                            list(filter(lambda x: x["type"] == "o", nominees)))  # select "o" towers from nominees
                     elif tower["type"] == "o":
                         nominees = [
                             area[row][column+1],
@@ -329,21 +243,21 @@ else:
                             area[row-1][column]]
                         attackers = list(
                             filter(lambda x: x["type"] == "+", nominees))
-                        how_much_touched_my_ass_count += 2*len(attackers)
-                        # print(
-                        #     f"{area[row][column]} took damage from {attackers=} {nominees=}")
-                    attacks[row][column] = how_much_touched_my_ass_count
+                        how_much_touched_me_count += 2*len(attackers)
+
+                    attacks[row][column] = how_much_touched_me_count
 
             for row in range(1, size+1):
                 for column in range(1, size+1):
+                    # deduce hitpoints
                     area[row][column]["health"] -= attacks[row][column]
+                    # remove + and o towers if they are dead
                     if area[row][column]["health"] <= 0:
                         area[row][column]["type"] = "."
                         area[row][column]['health'] = 0
+
+            # update part map for the next round
             part_map = [x[1:-1] for x in area[1:-1]]
-    comm.send(part_map, dest=0, tag=100)  # to print end of round
-
             # end of round
-
         # end of wave
-    # get new towers from manager
+    comm.send(part_map, dest=0, tag=100)  # to print end of wave
